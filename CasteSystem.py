2c18612e481eacde42011aa52ca717c0dc04ff38
Jmasterturbo1 <135137041+Jmasterturbo1@users.noreply.github.com>
2025-10-22 18:42:31 -0400

    Update CasteSystem.py

--- CasteSystem.py
+++ CasteSystem.py
@@ -93,12 +93,23 @@ async def changeblood(interaction: discord.Interaction, new_role: str):
             f"`{e}`, Could not be found, new blood type perhaps?",
             ephemeral=True
         )
-@bot.tree.command(name="tutorial", description="Gives descrption on what bot does and it's list of commands")
+@bot.tree.command(name="tutorial", description="Gives description on what the bot does and its list of commands")
 async def describe_me(interaction: discord.Interaction):
-    
-    tutorial_message ="--CASTESYSTEM.BOT HAS BEEN BOOTED. BEGINING TO RUN TUTORIAL--\n    Hello new user,\n    This bot has come equipped with several features to really customize your logs.\n    But first you need to do one thing.\n    You must assign a blood cast with any of these roles provided: {ROLE_NAMES}, using the command /changeblood to do so.\n    This bot also gets the Captail intials of your name, so if you have a name like EctoBiologist it will be changed to EB: when chatting.\n    And if you ever want to read the tutorial again you can use the command: /tutorial.\n--END OF TUTORIAL--"
-    
-    await interaction.response.send_message(f"\"{tutorial_message}\"")
+    tutorial_message = (
+        "--CASTESYSTEM.BOT HAS BEEN BOOTED. BEGINNING TO RUN TUTORIAL--\n"
+        "Hello new user,\n"
+        "This bot has come equipped with several features to really customize your logs.\n"
+        "But first you need to do one thing:\n"
+        f"You must assign a blood caste using one of these roles: {', '.join(ROLE_NAMES)}\n"
+        "Use the command `/changeblood` to do so.\n"
+        "This bot also gets the capital initials of your nickname, "
+        "so if you have a name like **EctoBiologist**, it will be changed to **EB:** when chatting.\n"
+        "And if you ever want to read this tutorial again, you can use the command `/tutorial`.\n"
+        "--END OF TUTORIAL--"
+    )
+
+    await interaction.response.send_message(tutorial_message, ephemeral=True)
+
 # ---------------- AUTOCOMPLETE ----------------
 @changeblood.autocomplete('new_role')
 async def new_role_autocomplete(interaction: discord.Interaction, current: str):
